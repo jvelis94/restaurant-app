fdf2cac6d20162483833450da5c20420a8076919
Joel Velis <joelvelis94@gmail.com>
2021-08-18 11:37:38 -0400

    added the ability to update quantity of cart items

--- store/order-context.js
+++ store/order-context.js
@@ -2,7 +2,9 @@ import React, { useState } from "react";
 
 const OrderContext = React.createContext({
     orders: [],
-    addToCart: (item) => {}
+    addToCart: (item) => {},
+    incrementQuantity: (item) => {},
+    decrementQuantity: (item) => {}
 });
 
 export const OrderContextProvider = (props) => {
@@ -20,12 +22,37 @@ export const OrderContextProvider = (props) => {
         }])
         console.log(orders)
     }
+    
+    const incrementQuantity = (item) => {
+        console.log(orders)
+        let orderItem = orders.find(order => order['name'] === item.name)
+        orderItem['quantity'] += 1
+        let updateIndex = orders.indexOf(item)
+        setOrders(prevState => {
+            prevState[updateIndex] = orderItem
+            return [...prevState]
+        })
+    }
+
+    const decrementQuantity = (item) => {
+        let updateIndex = orders.indexOf(item)
+        let orderItem = orders.find(order => order['name'] === item.name)
+        if (orderItem['quantity'] !== 1) {
+            orderItem['quantity'] -= 1
+            setOrders(prevState => {
+                prevState[updateIndex] = orderItem
+                return [...prevState]
+            })
+        }
+    }
 
     return (
         <OrderContext.Provider
             value={{
                 orders: orders,
-                addToCart: addToCart
+                addToCart: addToCart,
+                incrementQuantity: incrementQuantity,
+                decrementQuantity: decrementQuantity
             }}
         >
             {props.children}
